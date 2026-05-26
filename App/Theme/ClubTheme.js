b932ef74b120ee89a64bdbefff724fb2d9129802
ACB2310 <lrs22110100@purisima.tecnm.mx>
2026-05-25 21:17:06 -0600

    App Front creation classes

---
+++ App/Theme/ClubTheme.js
@@ -0,0 +1,23 @@
+export const CLUB_THEME = {
+  brandPrimary: {
+    garnet: "#A50044",
+    blue: "#004D98",
+    gold: "#EDBB00",
+    softGarnet: "#FCE7F3",
+    softBlue: "#E0EEFF",
+  },
+  brandSecondary: {
+    royalBlue: "#0033A0",
+    electricBlue: "#0057D8",
+    red: "#D71920",
+    white: "#FFFFFF",
+    softBlue: "#E8F0FF",
+  },
+  neutral: {
+    page: "#F4F6FB",
+    card: "#FFFFFF",
+    textPrimary: "#0F172A",
+    textSecondary: "#475569",
+    border: "#D7DFEA",
+  },
+};
